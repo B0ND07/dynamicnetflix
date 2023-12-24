149df189c5d92fe5c151964d5bd52385d9d26ee6
b0nd07 <mhashimkp@gmail.com>
2023-12-24 23:22:56 +0530

    nf: add preview show and buttons - preview show on top - overview of show - play and watch later buttons

--- Main.jsx
+++ Main.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function Main() {
     const [movies,setMovies]=useState([])
@@ -15,7 +16,21 @@ function Main() {
           });
       }, []);
   return (
-    <div>Main</div>
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+        <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movies[1].backdrop_path}`} alt={movies[1].title}/>
+      <div className='absolute w-full top-[20%] p-4 md:p-8'>
+        <h1 className='text-3xl md:text-5xl font-bold'>{movies[1].title}</h1>
+        <div className='my-4'>
+        
+        <button className='border px-4 py-2 bg-gray-300 text-black'>Play</button>
+        <button className='border px-4 py-2 bg-black-300 text-white ml-4'>Watch Later</button>
+        </div>
+        <p className='w-full md:max-w-[70%] lg:max-w-[50%]'>{movies[1].overview}</p>
+      </div>
+      </div>
+    </div>
   )
 }
 
