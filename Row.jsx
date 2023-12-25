46818f3f80480f22520f7da505d75e30fbe34238
b0nd07 <mhashimkp@gmail.com>
2023-12-25 23:09:31 +0530

    nf: add slider

--- Row.jsx
+++ Row.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa"; 
+import {MdChevronLeft, MdChevronRight} from "react-icons/md"
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, rowID }) {
   const [movies, setMovies] = useState([]);
   const [hoveredMovie, setHoveredMovie] = useState(null);
   const [like, setlike] = useState(false);
@@ -12,12 +13,24 @@ function Row({ title, fetchUrl }) {
       setMovies(response.data.results);
     });
   }, [fetchUrl]);
+  const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
 
   return (
     <div>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+      <div className="relative flex items-center group">
+
+        <MdChevronLeft onClick={slideLeft} className="bg-white rounded-full absolute  opacity-50 hover:opacity-100 cursor-pointer z-10 left-0 hidden group-hover:block" size={40}/>
+
+        <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
           {movies.map((display, id) => (
             <div
               key={id}
@@ -47,6 +60,7 @@ function Row({ title, fetchUrl }) {
             </div>
           ))}
         </div>
+        <MdChevronRight onClick={slideRight} className="bg-white rounded-full absolute  opacity-50 hover:opacity-100 cursor-pointer z-10 right-0 hidden group-hover:block" size={40}/>
       </div>
     </div>
   );
--- pages/Home.js
+++ pages/Home.js
@@ -6,10 +6,10 @@ function Home() {
   return (
     <div>
         <Main/>
-        <Row title='Trending Now' fetchUrl='https://api.themoviedb.org/3/trending/all/day?api_key=7375b3162ed1bb2f24bb965386019997'/>
-        <Row title='Top Rated' fetchUrl='https://api.themoviedb.org/3/movie/top_rated?api_key=7375b3162ed1bb2f24bb965386019997'/>
-        <Row title='Action' fetchUrl='https://api.themoviedb.org/3/discover/movie?api_key=7375b3162ed1bb2f24bb965386019997&with_genres=28'/>
-        <Row title='Comedy' fetchUrl='https://api.themoviedb.org/3/discover/movie?api_key=7375b3162ed1bb2f24bb965386019997&with_genres=35'/>
+        <Row rowID='1' title='Trending Now' fetchUrl='https://api.themoviedb.org/3/trending/all/day?api_key=7375b3162ed1bb2f24bb965386019997'/>
+        <Row rowID='2' title='Top Rated' fetchUrl='https://api.themoviedb.org/3/movie/top_rated?api_key=7375b3162ed1bb2f24bb965386019997'/>
+        <Row rowID='3' title='Action' fetchUrl='https://api.themoviedb.org/3/discover/movie?api_key=7375b3162ed1bb2f24bb965386019997&with_genres=28'/>
+        <Row rowID='4' title='Comedy' fetchUrl='https://api.themoviedb.org/3/discover/movie?api_key=7375b3162ed1bb2f24bb965386019997&with_genres=35'/>
         
     </div>
   )
