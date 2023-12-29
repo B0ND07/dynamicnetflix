0b92fb225be1878db24800df3dcf7776110d186b
b0nd07 <mhashimkp@gmail.com>
2023-12-29 23:27:52 +0530

    nf: set random movies on main page

--- Main.jsx
+++ Main.jsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 function Main() {
   const [movies, setMovies] = useState([]);
 
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
   useEffect(() => {
     axios
       .get(
@@ -23,13 +25,13 @@ function Main() {
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${
-            movies.length && movies[1].backdrop_path
+            movies.length && movie.backdrop_path
           }`}
           alt=""
         />
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">
-            {movies.length && movies[1].title}
+            {movies.length && movie.title}
           </h1>
           <div className="my-4">
             <button className="border px-4 py-2 bg-gray-300 text-black">
@@ -40,7 +42,7 @@ function Main() {
             </button>
           </div>
           <p className="w-full md:max-w-[70%] lg:max-w-[50%]">
-            {movies.length && movies[1].overview}
+            {movies.length && movie.overview}
           </p>
         </div>
       </div>
